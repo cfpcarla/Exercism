8f47c60f325313619c31b64c302e928827a34714
Carla <cfpcarla@gmail.com>
2021-02-25 10:51:10 -0500

    Submited the exercise

---
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -0,0 +1,25 @@
+//
+// This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const decodedValue = ([color1, color2]) => {
+  return getIndex(color1) * 10 + getIndex(color2);
+};
+
+const getIndex = (color) => {
+  return COLORS.indexOf(color);
+};
+
+const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
